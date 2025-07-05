9394aeb0de54e93d9861ef2f476b06fdad9252e6
Akachukwu <okoli555aka@gmail.com>
2025-07-05 12:06:18 +0100

    fix

--- src/components/partners_logos_display.tsx
+++ src/components/partners_logos_display.tsx
@@ -7,7 +7,7 @@ const PartnersLogosDisplay = () => {
   const {isLogosLoading, logos} = useAppData();
 
   return (
-    <section className={`w-full ${(!isLogosLoading && logos.length > 0) ? 'py-[3.75rem]' : 'pb-[6rem] 1sm:pb-[7.5rem]'} overflow-hidden`}>
+    <section className={`w-full ${(isLogosLoading || logos.length > 0) ? 'py-[3.75rem]' : 'pb-[6rem] 1sm:pb-[7.5rem]'} overflow-hidden`}>
         <div className="w-full px-[1rem] 1sm:px-[1.5rem] flex justify-center">
           <div className='flex flex-col w-full max-w-[67.25rem] items-center gap-[3rem]'>
             <motion.h2 className='opacity-[.6] uppercase text-primary text-[.875rem]/[1.25rem] 3sm:text-[1rem]/[1.25rem] tracking-[2px]'
